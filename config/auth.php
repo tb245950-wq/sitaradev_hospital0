0eb1d12953a6aa084ca59c3b5cbc1b7d6657d774
RootAcc <muhammadmughni@student.plai.ac.id>
2026-06-12 10:00:20 +0700

    change guards in folder config/auth.php

--- config/auth.php
+++ config/auth.php
@@ -38,11 +38,16 @@ return [
     */
 
     'guards' => [
-        'web' => [
-            'driver' => 'session',
-            'provider' => 'users',
-        ],
+    'web' => [
+        'driver' => 'session',
+        'provider' => 'users',
     ],
+
+    'api' => [
+        'driver' => 'sanctum', // ← WAJIB 'sanctum', bukan 'token'
+        'provider' => 'users',
+    ],
+],
 
     /*
     |--------------------------------------------------------------------------
