60311e835f70f9f586dbb20db7e420fc2957ffa1
RootAcc <muhammadmughni@student.plai.ac.id>
2026-06-12 11:03:55 +0700

    add api.php queu Routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\PatientController;
+use App\Http\Controllers\Api\QueueController;
 use Illuminate\Support\Facades\Route;
 
 // Public routes (tanpa token)
@@ -13,7 +14,11 @@ Route::middleware('auth:sanctum')->group(function () {
     // Auth routes
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/user', [AuthController::class, 'user']);
-    
-    // Patient CRUD routes (FR-03, FR-04, FR-05, FR-14)
+
+    // Patient CRUD routes
     Route::apiResource('patients', PatientController::class);
+
+    // Queue routes
+    Route::apiResource('queues', QueueController::class);
+    Route::post('/queues/call-next', [QueueController::class, 'callNext']);
 });
